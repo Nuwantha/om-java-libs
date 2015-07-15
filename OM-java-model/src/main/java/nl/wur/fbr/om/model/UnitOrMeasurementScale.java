c29a0e6dd0e291a40c12f24c35aa72a17c01a666
Dieudonné Willems <don.willems@wur.nl>
2015-07-15 13:54:15 +0200

    Adds an identifier string to all units and measurement scales
    This identifier is to be used to test for equality. If the implementation does not support custom identifiers, a unique identifier should be constructed.

--- OM-java-model/src/main/java/nl/wur/fbr/om/model/UnitOrMeasurementScale.java
+++ OM-java-model/src/main/java/nl/wur/fbr/om/model/UnitOrMeasurementScale.java
@@ -6,4 +6,11 @@ package nl.wur.fbr.om.model;
  * @author Don Willems on 14/07/15.
  */
 public interface UnitOrMeasurementScale {
+
+    /**
+     * Returns the identifier for the specific unit or measurement scale. This identifier should be unique
+     * and should be used to compare units or measurements scales for equality.
+     * @return The identifier for the unit or measurement scale.
+     */
+    public String getIdentifier();
 }
