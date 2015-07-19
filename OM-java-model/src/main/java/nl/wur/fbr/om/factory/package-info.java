1bdcc595b7e63a5e648094fdc4c66672bd53cda5
Dieudonné Willems <don.willems@wur.nl>
2015-07-19 12:14:07 +0200

    Added create methods with names
    Added create methods for units and scale that also take a name, symbol, and identifier.

--- OM-java-model/src/main/java/nl/wur/fbr/om/factory/package-info.java
+++ OM-java-model/src/main/java/nl/wur/fbr/om/factory/package-info.java
@@ -1,5 +1,7 @@
 /**
- * This package contains the factory interfaces used for creating units and measurement scales.
+ * This package contains the factory interfaces used for creating units and measurement scales
+ * and for converting units and scales between each other. It also contains the different
+ * exception classes that can be thrown by these methods.
  *
  * @since v0.2.0
  */
--- OM-java-model/src/main/java/nl/wur/fbr/om/model/SingularUnit.java
+++ OM-java-model/src/main/java/nl/wur/fbr/om/model/SingularUnit.java
@@ -16,6 +16,8 @@ public interface SingularUnit extends Unit {
      * Returns the Unit on which the unit is based.
      * For instance, the unit Astronomical Unit (AU) is defined as
      * 1.495978707e11 metre. The definition unit is in this case the metre.
+     * This method may also return null if no definition unit is defined for this unit, i.e. when this unit
+     * is a base unit in some system of units. The metre is, for instance, a base unit in SI.
      * @return The unit that is used as the definition unit.
      */
     public Unit getDefinitionUnit();
@@ -24,6 +26,8 @@ public interface SingularUnit extends Unit {
      * Returns the numerical conversion factor to covert between this unit and its definition unit.
      * For instance, the unit Astronomical Unit (AU) is defined as
      * 1.495978707e11 metre. The numerical value is in this case: 1.495978707e11.
+     * If no definition unit is set (i.e. this unit is a base unit for a system of units), this method
+     * should return 1.
      * @return The numerical value used to convert between this unit and its base.
      */
     public double getDefinitionNumericalValue();
