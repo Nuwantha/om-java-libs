7a51183993254b57ab8cdedba22654effadb7924
Dieudonné Willems <don.willems@wur.nl>
2015-07-22 02:44:35 +0200

    Removed Tensor and Vecor interfaces
    Vectors are now defined using an array of numbers.

--- OM-java-model/src/main/java/nl/wur/fbr/om/model/values/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains interfaces that define several numerical value types such as
- * vectors and tensors. Scalars are implemented as instances of {@link java.lang.Number}.
- * These numerical values can be added to {@link nl.wur.fbr.om.model.measures.Measure measures} or
- * {@link nl.wur.fbr.om.model.points.Point points} on a measurement scale.
- * @author Don Willems on 21/07/15.
- */
-package nl.wur.fbr.om.model.values;
